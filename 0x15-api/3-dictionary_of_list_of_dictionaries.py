283a6b426f60b6a85dc05bd4566a5ac49e4d20f6
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-27 21:07:02 +0000

    initial commit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import json
+"""
+accessing a url with employee ID to return information
+"""
+
+
+if __name__ == "__main__":
+    users = requests.get("https://jsonplaceholder.typicode.com/users",
+                        verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
+                        verify=False).json()
+    user_dict = {}
+    usernamedict = {}
+    for user in users:
+        ID = user.get("id")
+        user_dict[ID] = []
+        usernamedict[ID] = user.get('username')
+    for task in todo:
+        task_dict = {}
+        ID = task.get("userId")
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict['username'] = usernamedict.get(ID)
+        user_dict.get(ID).append(task_dict)
+    with open("todo_all_employees.json", 'w') as jsfile:
+        json.dump(user_dict, jsfile)
