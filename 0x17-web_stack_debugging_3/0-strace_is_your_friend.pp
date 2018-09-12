361a3f0e3a5283b49c339a4895603c1d94568b76
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-12 20:54:34 +0000

    sed global command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Replacing right php config files to load php
 exec {'replacing_right path':
-  command => 'sed -i "s/.phpp/.php"  /var/www/html/wp-settings.php',
+  command => 'sed -i "s/.phpp/.php/g"  /var/www/html/wp-settings.php',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => 'test -f /var/www/html/wp-settings.php'
 }
