a2ed8cd4e2c28b36611ac74b9658477695f6c873
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-20 23:32:37 +0000

    removing regex

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Setting upper limit for the pinging requests
 exec { 'upperlimit' :
-     command =>  'sed -r -i "s/(ULIMIT =\"-n) [0-9]+/\1 10000" /etc/default/nginx; service nginx restart',
+     command =>  'sed -i "s/15/10000/g" /etc/default/nginx; service nginx restart',
      path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
      onlyif  =>  'test -e /etc/default/nginx',
 }
